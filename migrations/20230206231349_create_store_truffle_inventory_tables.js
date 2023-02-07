d76420858778b31aac23b8beb80f6129ee35bb87
SaagarSuresh <saagarsuresh92@gmail.com>
2023-02-06 19:44:42 -0500

    Edit migration file to create 'store', 'truffle', 'inventory' and 'notifications' table.

--- migrations/20230206231349_create_store_truffle_inventory_tables.js
+++ migrations/20230206231349_create_store_truffle_inventory_tables.js
@@ -1,9 +1,53 @@
 exports.up = function(knex) {
   return knex.schema
-    
+    .createTable('store', (table) => {
+        table.uuid('id').primary();
+        table.string('name').notNullable();
+        table.timestamps(true,true);
+    })
+    .createTable('truffle', (table) =>{
+        table.uuid('id').primary();
+        table.string('name').notNullable();
+        table.timestamps(true,true);
+    })
+    .createTable('inventory', (table) =>{
+        table.uuid('id').primary();
+        table
+            .uuid('store_id')
+            .references('store.id')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE');
+        table
+            .uuid('truffle_id')
+            .references('truffe.id')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE');
+        table
+            .integer('minimum_truffles_in_container')
+            .unsigned()
+            .notNullable()
+        table
+            .integer('truffles_in_container')
+            .unsigned()
+            .notNullable()
+        table
+            .integer('truffles_sold')
+            .unsigned()
+        table.timestamps(true,true);
+    })
+    .createTable('notifications', (table) =>{
+        table.uuid('id').primary();
+        table
+            .uuid('inventory_id')
+            .references('inventory.id')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE');
+        table.string('alert_message').notNullable();
+        table.timestamps(true,true);
+    });
 };
 
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('notifications').dropTable('inventory').dropTable('truffle').dropTable('store');
 };
