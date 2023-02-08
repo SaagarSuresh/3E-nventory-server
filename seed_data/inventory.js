ff52a466bdee65aac6458a28413a5c4108b8fed7
SaagarSuresh <saagarsuresh92@gmail.com>
2023-02-08 00:31:49 -0500

    Create store, truffle and inventory .js files and add dummy content.

---
+++ seed_data/inventory.js
@@ -0,0 +1,74 @@
+module.exports = [
+    {
+        id: '226fd4a6-863c-459d-b69c-007262a64015',
+        store_id: '2922c286-16cd-4d43-ab98-c79f698aeab0',
+        truffle_id: '72c826ba-fde0-4aae-9aaf-95c6072946cd',
+        minimum_truffles_in_container: 0,
+        truffles_in_container: 0,
+        truffles_sold: 0
+    },
+    {
+        id: '0716ecfc-e296-4f9f-906b-45e84030285b',
+        store_id: '2922c286-16cd-4d43-ab98-c79f698aeab0',
+        truffle_id: 'e202e167-d242-4d7f-b8fc-68caffef9e01',
+        minimum_truffles_in_container: 0,
+        truffles_in_container: 0,
+        truffles_sold: 0
+    },
+    {
+        id: '9fe106e0-d719-4963-9f5b-c9321b786064',
+        store_id: '2922c286-16cd-4d43-ab98-c79f698aeab0',
+        truffle_id: '72c826ba-fde0-4aae-9aaf-95c6072946cd',
+        minimum_truffles_in_container: 0,
+        truffles_in_container: 0,
+        truffles_sold: 0
+    },
+    {
+        id: 'f516e1c9-486a-40c2-9bed-c8eba070f1f6',
+        store_id: '5bf7bd6c-2b16-4129-bddc-9d37ff8539e9',
+        truffle_id: '8f16bd30-bab5-40af-aca2-b63d5fdd1acc',
+        minimum_truffles_in_container: 0,
+        truffles_in_container: 0,
+        truffles_sold: 0
+    },
+    {
+        id: '4b6a7565-077e-4595-a317-c53095fd5dad',
+        store_id: '5bf7bd6c-2b16-4129-bddc-9d37ff8539e9',
+        truffle_id: 'e202e167-d242-4d7f-b8fc-68caffef9e01',
+        minimum_truffles_in_container: 0,
+        truffles_in_container: 0,
+        truffles_sold: 0
+    },
+    {
+        id: '9abbf2d2-45d5-463c-8429-ca5454d971d4',
+        store_id: '5bf7bd6c-2b16-4129-bddc-9d37ff8539e9',
+        truffle_id: '2c0185c7-89ef-48ad-a22f-9fc022a5c90c',
+        minimum_truffles_in_container: 0,
+        truffles_in_container: 0,
+        truffles_sold: 0
+    },
+    {
+        id: '5c2000b8-f8c4-461f-935c-66eaeb52c561',
+        store_id: '90ac3319-70d1-4a51-b91d-ba6c2464408c',
+        truffle_id: 'f19b0b8a-9cca-4567-9af7-4016a15e038a',
+        minimum_truffles_in_container: 0,
+        truffles_in_container: 0,
+        truffles_sold: 0
+    },
+    {
+        id: '462e4097-ae85-4771-ba42-d28393e39e03',
+        store_id: '90ac3319-70d1-4a51-b91d-ba6c2464408c',
+        truffle_id: 'bdc6bb69-e09c-498d-8abd-be2792504d00',
+        minimum_truffles_in_container: 0,
+        truffles_in_container: 0,
+        truffles_sold: 0
+    },
+    {
+        id: 'ae986f59-8c40-480c-bd28-ee6068cd3617',
+        store_id: '90ac3319-70d1-4a51-b91d-ba6c2464408c',
+        truffle_id: '9b4f79ea-0e6c-4e59-8e05-afd933d0b3d3',
+        minimum_truffles_in_container: 0,
+        truffles_in_container: 0,
+        truffles_sold: 0
+    }
+]
---
+++ seed_data/truffle.js
@@ -0,0 +1,133 @@
+module.exports = [
+    {
+        id: '9b4f79ea-0e6c-4e59-8e05-afd933d0b3d3',
+        name: 'Snowball - Plain White'
+    },
+    {
+        id: '83433026-ca32-4c6d-bd86-a39ee8b7303e',
+        name: 'Mimosa - Orange Juice & Champagne'
+    },
+    {
+        id: 'a193a6a7-42ab-4182-97dc-555ee85e7486',
+        name: 'Bluebell - Blueberry'
+    },
+    {
+        id: '8f16bd30-bab5-40af-aca2-b63d5fdd1acc',
+        name: 'Laurier - Strawberry'
+    },
+    {
+        id: 'bdc6bb69-e09c-498d-8abd-be2792504d00',
+        name: 'Princess Sophie - Almond Butter'
+    },
+    {
+        id: '3ce124a4-78b0-4d80-91b9-11f9ced631a7',
+        name: 'Singh - Lemon'
+    },
+    {
+        id: '4dd464d6-90b8-4330-91e0-bd1217811bd9',
+        name: 'Rainbow - Passion Fruit'
+    },
+    {
+        id: 'c0ba64a8-0ecb-4a7d-ab7f-2dbbd1d437b1',
+        name: 'Milky Way - Plain Milk'
+    },
+    {
+        id: 'd9ef9352-2367-4272-8884-70cf80cb7a38',
+        name: 'Shamrock - Baileys'
+    },
+    {
+        id: 'b70067d1-49c9-4925-b830-67b3e98005d5',
+        name: 'Nutty Irishman - Baileys & Frangelico'
+    },
+    {
+        id: '7c79d334-6b90-4052-9def-aa9b8519c9fb',
+        name: 'Bubbly - Champagne'
+    },
+    {
+        name: 'Algonquin - Almond Butter'
+    },
+    {
+        id: 'eafcb711-a726-4b3c-adec-704e3265c47d',
+        name: 'Nomad - Sea Salt & Caramel'
+    },
+    {
+        id: '72c826ba-fde0-4aae-9aaf-95c6072946cd',
+        name: 'Purple Rain - Lavender'
+    },
+    {
+        id: '019da03d-1162-48a4-ad48-ed655e3d7301',
+        name: 'Dark Knight - Plain Dark'
+    },
+    {
+        id: '705b808f-d8a0-4713-a796-8653c5c5952b',
+        name: 'Fleur De Lis - Brandy'
+    },
+    {
+        id: 'f19b0b8a-9cca-4567-9af7-4016a15e038a',
+        name: 'Simryn - Caramel'
+    },
+    {
+        id: '2cba0296-c77d-4758-9115-bd98ac18f2c0',
+        name: 'Mr. Bean - Java & Kahlua'
+    },
+    {
+        id: 'e202e167-d242-4d7f-b8fc-68caffef9e01',
+        name: 'Great Dane - Cherry Wine'
+    },
+    {
+        id: '079c88df-ed32-4d88-a9b7-eaebc85c4ce2',
+        name: 'Contessa - Cognac'
+    },
+    {
+        id: '56c8e94c-777d-4176-b7fd-02f0806f614a',
+        name: 'Monte Cristo - Grand Marnier'
+    },
+    {
+        id: '2c0185c7-89ef-48ad-a22f-9fc022a5c90c',
+        name: 'Island Girl - Cconut Rum & Flakes'
+    },
+    {
+        id: 'f3e13429-b5e9-4a50-b01c-75fb07cefded',
+        name: 'Blackbird - Raspberry'
+    },
+    {
+        id: '3b042051-f18c-4a16-a393-68ceba87277a',
+        name: 'Gandhi - Caramel Filing & Sea Salt on top'
+    },
+    {
+        id: '4d6ce289-eb58-45a1-bae3-e3874290ee48',
+        name: 'Carnival - Gianduja & White Chcolate'
+    },
+    {
+        id: 'd3c08f85-4570-48e1-88df-610614477359',
+        name: 'Amazonian - Hazelnut Paste, Giaduja '
+    },
+    {
+        id: '043a70e2-cf28-4ce1-a9a4-e5b3fac9c593',
+        name: 'Union Jack - Almond Butter'
+    },
+    {
+        id: '66327933-ce38-47e0-9bfe-5d29048bacae',
+        name: 'Wanderlust - Caramel, Seasalt & dark chocolate'
+    },
+    {
+        id: '70ca48a6-93e2-4ff0-b3a2-f5a38f1a043e',
+        name: 'Del Toro - Chilli'
+    },
+    {
+        id: 'e02f302a-c67c-4600-bf12-e364b59b80ea',
+        name: 'True North - Vanilla'
+    },
+    {
+        id: 'd3d7aa3c-6e27-4ab0-9496-f95faa5042f0',
+        name: 'Valhalla - Peppermint'
+    },
+    {
+        id: '828c12d1-c0dd-4fd4-b8db-ac8eb493f8f8',
+        name: 'Jack Sparrow - Bourbon'
+    },
+    {
+        id: 'b6abe52c-05be-4c90-b39a-d9decefd4274',
+        name: 'Key West - Key Lime'
+    }
+]
