3c06ad057803517b6bf087fe2b2e6b3fd6d91b41
SaagarSuresh <saagarsuresh92@gmail.com>
2023-02-15 19:42:45 -0500

    Edit seed_data file. Add scripts.

--- seed_data/inventory.js
+++ seed_data/inventory.js
@@ -3,33 +3,33 @@ module.exports = [
         id: '226fd4a6-863c-459d-b69c-007262a64015',
         store_id: '2922c286-16cd-4d43-ab98-c79f698aeab0',
         truffle_id: '72c826ba-fde0-4aae-9aaf-95c6072946cd',
-        minimum_truffles_in_container: 0,
-        truffles_in_container: 0,
-        truffles_sold: 0
+        minimum_truffles_in_container: 10,
+        truffles_in_container: 15,
+        truffles_sold: 2
     },
     {
         id: '0716ecfc-e296-4f9f-906b-45e84030285b',
         store_id: '2922c286-16cd-4d43-ab98-c79f698aeab0',
         truffle_id: 'e202e167-d242-4d7f-b8fc-68caffef9e01',
-        minimum_truffles_in_container: 0,
-        truffles_in_container: 0,
-        truffles_sold: 0
+        minimum_truffles_in_container: 20,
+        truffles_in_container: 35,
+        truffles_sold: 5
     },
     {
         id: '9fe106e0-d719-4963-9f5b-c9321b786064',
         store_id: '2922c286-16cd-4d43-ab98-c79f698aeab0',
         truffle_id: '72c826ba-fde0-4aae-9aaf-95c6072946cd',
-        minimum_truffles_in_container: 0,
-        truffles_in_container: 0,
-        truffles_sold: 0
+        minimum_truffles_in_container: 15,
+        truffles_in_container: 30,
+        truffles_sold: 10
     },
     {
         id: 'f516e1c9-486a-40c2-9bed-c8eba070f1f6',
         store_id: '5bf7bd6c-2b16-4129-bddc-9d37ff8539e9',
         truffle_id: '8f16bd30-bab5-40af-aca2-b63d5fdd1acc',
-        minimum_truffles_in_container: 0,
-        truffles_in_container: 0,
-        truffles_sold: 0
+        minimum_truffles_in_container: 15,
+        truffles_in_container: 25,
+        truffles_sold: 6
     },
     {
         id: '4b6a7565-077e-4595-a317-c53095fd5dad',
@@ -43,17 +43,17 @@ module.exports = [
         id: '9abbf2d2-45d5-463c-8429-ca5454d971d4',
         store_id: '5bf7bd6c-2b16-4129-bddc-9d37ff8539e9',
         truffle_id: '2c0185c7-89ef-48ad-a22f-9fc022a5c90c',
-        minimum_truffles_in_container: 0,
-        truffles_in_container: 0,
-        truffles_sold: 0
+        minimum_truffles_in_container: 20,
+        truffles_in_container: 34,
+        truffles_sold: 7
     },
     {
         id: '5c2000b8-f8c4-461f-935c-66eaeb52c561',
         store_id: '90ac3319-70d1-4a51-b91d-ba6c2464408c',
         truffle_id: 'f19b0b8a-9cca-4567-9af7-4016a15e038a',
-        minimum_truffles_in_container: 0,
-        truffles_in_container: 0,
-        truffles_sold: 0
+        minimum_truffles_in_container: 20,
+        truffles_in_container: 33,
+        truffles_sold: 12
     },
     {
         id: '462e4097-ae85-4771-ba42-d28393e39e03',
@@ -67,8 +67,8 @@ module.exports = [
         id: 'ae986f59-8c40-480c-bd28-ee6068cd3617',
         store_id: '90ac3319-70d1-4a51-b91d-ba6c2464408c',
         truffle_id: '9b4f79ea-0e6c-4e59-8e05-afd933d0b3d3',
-        minimum_truffles_in_container: 0,
-        truffles_in_container: 0,
-        truffles_sold: 0
+        minimum_truffles_in_container: 25,
+        truffles_in_container: 29,
+        truffles_sold: 2
     }
 ]
